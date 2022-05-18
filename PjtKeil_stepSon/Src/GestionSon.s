5e58db87ba43c13bb634af88fb58ee4798ba6c20
asmun85 <asmunkanane1@gmail.com>
2022-05-18 14:21:24 +0200

    BE CHTI projet final

---
+++ PjtKeil_stepSon/Src/GestionSon.s
@@ -0,0 +1,83 @@
+	PRESERVE8
+	THUMB   
+		
+
+; ====================== zone de réservation de données,  ======================================
+;Section RAM (read only) :
+	area    mesdata,data,readonly
+
+
+;Section RAM (read write):
+	area    maram,data,readwrite
+		
+;On declare variable globale
+SortieSon DCW 0 ; 16 bit
+Index DCD 0 ;32bit
+	
+	EXPORT SortieSon
+		
+	IMPORT Son
+	IMPORT LongueurSon
+	IMPORT PeriodeSonMicroSec
+	
+; ===============================================================================================
+	
+
+	EXPORT CallbackSon
+	EXPORT StartSon
+		
+	INCLUDE ./Driver/DriverJeuLaser.inc
+		
+;Section ROM code (read only) :		
+	area    moncode,code,readonly
+; écrire le code ici		
+
+
+
+CallbackSon proc
+	PUSH{LR,R5}
+	LDR R1,=Index
+	LDR R0,=LongueurSon
+	LDR R3,[R1]
+	LDR R2,[R0]
+	;if(index<5512) then
+	CMP R3,R2
+	BHS then
+
+; inferieur
+els
+		;sonBrut = Son[index];
+		; 16 bit
+		LDR R2,=Son
+		LDR R4,=SortieSon
+		LDRSH R0,[R2,R3,LSL #1]
+		; index++;
+		ADD R3,#1
+		STR R3,[R1]
+		;SortieSon = mise_a_echelle(sonBrut)
+		; sonBrut += 32768
+		ADD R0,#32768
+		; SortieSon = sonBrut /  92 ! attention conversion
+		MOV R5,#92
+		UDIV R0,R5 ;R3
+		STRH R0,[R4]
+		bl PWM_Set_Value_TIM3_Ch3
+		B finsi
+		
+then
+	bl StartSon
+		
+finsi ;superieur
+	POP {PC,R5}
+
+
+	endp
+		
+StartSon proc
+	LDR R0,=Index
+	MOV R1,#0
+	STR R1,[R0]
+	bx lr
+	endp
+		
+	END	
---
+++ PjtKeil_stepSon/Src/principal.c
@@ -0,0 +1,35 @@
+
+
+#include "DriverJeuLaser.h"
+
+#define PeriodeSonSec 0.00000091
+
+extern void CallbackSon(void);
+extern int periodeSonMicroSec;
+
+int main(void)
+{
+
+// ===========================================================================
+// ============= INIT PERIPH (faites qu'une seule fois)  =====================
+// ===========================================================================
+
+// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
+CLOCK_Configure();
+Timer_1234_Init_ff( TIM4, 72*91);
+Active_IT_Debordement_Timer( TIM4, 2, CallbackSon );
+PWM_Init_ff(TIM3,3,720); // Periode 100µs Frequence 100kHz
+
+GPIO_Configure(GPIOB, 0, OUTPUT, ALT_PPULL);
+
+	
+	
+
+//============================================================================	
+	
+	
+while	(1)
+	{
+	}
+}
+
